109e91fd3e42082bacb528ae7662db637bb178a5
shashi <shashi.kanduri@outlook.com>
2024-11-26 09:20:33 -0800

    wip

--- frontend/src/components/MainContainer.jsx
+++ frontend/src/components/MainContainer.jsx
@@ -1,3 +1,5 @@
+import Playlist from "./Playlist";
+
 
 function MainContainer() {
 
@@ -7,13 +9,7 @@ function MainContainer() {
       <div className="w-[80%] bg-white rounded-xl h-fit overflow-hidden flex flex-col px-4 md:px-8 py-6">
         <h1 className = "font-bold text-xl"> Your Playlists </h1>
         <div className="text-sm md:text-base font-semibold my-4 text-gray-600">
-          <h2>
-            Spotify playlists come to life! Log in with your Spotify account to unlock powerful playlist analysis
-            powered by advanced language models. Discover trends, gain insights, and enhance your listening experience like never before.
-            Ready to explore the music behind the numbers? Log in now and start analyzing!"
-            <br />
-            <br />
-          </h2>
+          <Playlist />
           <br />
         </div>
       </div>
---
+++ frontend/src/components/Playlist.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Playlist() {
+  return (
+    <div className = 'w-full flex justify-between p-8'>
+
+        {/* playlist image and name */}
+        <div className = 'w-[20%] flex'>
+            <img></img>
+            <p>Name</p>
+        </div>
+
+        <a>Open on Spotify</a>
+    </div>
+  )
+}
+
+export default Playlist
